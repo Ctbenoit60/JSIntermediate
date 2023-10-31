259d7dcc510ea26b2bc9488e3aa3972f5a29471f
Ctbenoit60 <ctbenoit60@gmail.com>
2023-10-30 19:52:41 -0500

    adding the second funciton for part 2

--- index2.js
+++ index2.js
@@ -12,5 +12,18 @@ function truncate(str,max) {
 
 }
 
+function otherTruncate(str,max) {
+    if (str.length >= max) {
+        return str.slice(0, max) + "...";
+    } else {
+        return str;
+    }
+}
+function length(string) {
+    return string.slice(' ').length;
+}
+
 console.log(truncate('This text will be truncated if it is too long', 25))
+console.log(otherTruncate('how much wood could a woodchuck chuck', 32));
+console.log(length('how much wood could a woodchuck chuck'));
 // This text will be truncat...
