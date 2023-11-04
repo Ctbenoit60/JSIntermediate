5cb0a521bff3dd061781388ac424a2850f7e547c
Ctbenoit60 <ctbenoit60@gmail.com>
2023-11-04 08:38:20 -0500

    answering question a,b, & c

--- index8.js
+++ index8.js
@@ -4,9 +4,35 @@ const phoneBookABC = new Map() //an empty map to begin with
 phoneBookABC.set('Annabelle', '0412312343')
 phoneBookABC.set('Barry', '0433221117')
 phoneBookABC.set('Caroline', '0455221182')
+
+
 // a) Create a new phoneBookDEF Map to store names beginning with D, E or F
 // b) Initialise the contents of phoneBookDEF by passing in an array of keys/values
+
+const phoneBookDEF = new Map([
+
+        ['Daniel', '7657854448'],
+        ['Edith', '9878978898'],
+       ['Freddrick', '3456543456']
+      ]
+) 
+
+for (let contacts of phoneBookDEF.keys()){
+    console.log(Array(contacts));
+}
+
+for (let phoneNumbers of phoneBookDEF.values()){
+    console.log(Array(phoneNumbers));
+}
+
+
 // c) Update the phone number for Caroline
+
+phoneBookABC.set('Caroline', '7705542772')
+
+console.log(phoneBookABC);
+
+
 // d) Write a function printPhoneBook(contacts) that prints the names and phone
 // numbers in the given Map
 // e) Combine the contents of the two individual Maps into a single phoneBook Map
