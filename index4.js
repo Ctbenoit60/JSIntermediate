ce5165e7aad6c07b45cd6d3bd0826d4f937eb94c
Ctbenoit60 <ctbenoit60@gmail.com>
2023-11-04 13:10:44 -0500

    added a for loop

--- index4.js
+++ index4.js
@@ -5,25 +5,47 @@
 // b) Create variants of the camelCase function that use different types of for loops, and
 // c) with and without the conditional operator.
 
-// function camelCase(cssProp) {
-//   cssProp = cssProp.split("-");
-//   if (cssProp.length >= 2) {
-//     const cssProp2 = cssProp[1];
-//     noDash = cssProp2.charAt(0).toUpperCase(1) + cssProp.slice(1);
-//   }
-//   return console.log(noDash);
-// }
+function camelCase(cssProp) {
+  const words = cssProp.split("-");
+  console.log(words);
+  let string = "";
+  words.forEach((word) => {
+    console.log(word);
+    if (string.length === 0) {
+      string = word;
+    } else {
+      string += word.charAt(0).toUpperCase() + word.substring(1);
+    }
+  });
+  return string;
+}
 
 //part a: Write a function camelCase(cssProp) that changes dash-separated CSS properties like
 // 'margin-left' into camel-cased 'marginLeft'.
-function camelCase(cssProp) {
-    cssProp = cssProp.split('-')
-    for (let i = 1; i < cssProp.length; i++) {
-        cssProp[i] = cssProp[i][0].toUpperCase() + cssProp[i].slice(1);
-      }
-   return cssProp.join('')
+// function camelCase(cssProp) {
+//     cssProp = cssProp.split('-')
+//     for (let i = 1; i < cssProp.length; i++) {
+//         cssProp[i] = cssProp[i][0].toUpperCase() + cssProp[i].slice(1);
+//       }
+//    return cssProp.join('')
+//   }
+
+function mooseCase(cssProp) {
+  const corgi = cssProp.split("-");
+  // let string ='';
+  let dog = "";
+  if (corgi.length === 1) {
+    return cssProp;
   }
-  
-console.log(camelCase("margin-left")); // marginLeft
-console.log(camelCase("background-image")); // backgroundImage
-console.log(camelCase("display")); // display
+  for (let i = 0; i < corgi.length; i++) {
+    if (i === 1) {
+      dog = corgi[0] + corgi[i][0].toUpperCase() + corgi[i].substring(1);
+    }
+    // else if index = 0 then print cssProp
+  }
+  return dog;
+}
+
+console.log(mooseCase("margin-left")); // marginLeft
+console.log(mooseCase("background-image")); // backgroundImage
+console.log(mooseCase("display")); // display
